f7a27092c104678f12bb253bbfb6eced0e731662
Balaji-Nandure <balajirn.2016@gmail.com>
2026-09-23 16:58:17 +0530

    environment variables passing and port binidnig

--- chapter1/server.js
+++ chapter1/server.js
@@ -1,28 +1,15 @@
-// Import the Express library.
 const express = require("express");
 
-// Create an Express application.
 const app = express();
 
-// We will run our server on port 3000 inside the container.
-const PORT = 3000;
+// Use PORT environment variable if provided.
+// Otherwise default to 3000.
+const PORT = process.env.PORT || 3000;
 
-// Create a simple GET route.
 app.get("/", (req, res) => {
-  res.send("docker is working!2");
+  res.send(`Server running on port ${PORT}`);
 });
 
-// Start the server.
-//
-// Important:
-// We use "0.0.0.0" instead of only localhost.
-//
-// Why?
-// Because inside Docker, binding only to 127.0.0.1 can make
-// the application inaccessible from outside the container.
-//
-// 0.0.0.0 means:
-// "Listen on all available network interfaces."
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
